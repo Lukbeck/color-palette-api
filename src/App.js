19759324525adb7dff95e726cdabdfd662c91911
AlwinSunil <alv12alwin@gmail.com>
2021-08-21 20:30:10 +0530

    Basic functionalities implemented.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
 
 function App() {
+  const [colors, setcolors] = useState();
+
+  const randomPalette = () => {
+    axios({
+      method: "POST",
+      url: " https://cors-everywhere.herokuapp.com/http://colormind.io/api/",
+      data: {
+        model: "ui",
+      },
+    })
+      .then((res) => {
+        setcolors(res.data.result);
+        console.log(res.data.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    axios({
+      method: "POST",
+      url: " https://cors-everywhere.herokuapp.com/http://colormind.io/api/",
+      data: {
+        model: "ui",
+      },
+    })
+      .then((res) => {
+        setcolors(res.data.result);
+        console.log(res.data.result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <h2>Color Palette Generator</h2>
+      </div>
+      <div className="palette">
+        {colors ? (
+          <>
+            {colors.map((color) => (
+              <div className="color-card">
+                <div
+                  className="color-element"
+                  style={{ backgroundColor: `rgb(${color})` }}
+                ></div>
+                <div className="color-code" onClickCapture>
+                  <p>
+                    rgb({color[0]},
+                    {color[1]},
+                    {color[2]})
+                  </p>
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+            <div className="color-card">
+              <div className="color-element"></div>
+              <div className="color-code"></div>
+            </div>
+            <div className="color-card">
+              <div className="color-element"></div>
+              <div className="color-code"></div>
+            </div>
+            <div className="color-card">
+              <div className="color-element"></div>
+              <div className="color-code"></div>
+            </div>
+            <div className="color-card">
+              <div className="color-element"></div>
+              <div className="color-code"></div>
+            </div>
+            <div className="color-card">
+              <div className="color-element"></div>
+              <div className="color-code"></div>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="action">
+        <button onClick={randomPalette}>Generate Palette</button>
+        <p>Or just press the "Spacebar" to Generate palette.</p>
+      </div>
+      <div className="help">
+        <p> Click to copy individual color</p>
+        <p className="key">&nbsp;•&nbsp;</p>
+        <p className="key">Press "C" to copy palettes</p>
+      </div>
     </div>
   );
 }
